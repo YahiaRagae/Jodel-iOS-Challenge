e067b378c20992a920f86188117e1b866bfa58d5
Yahia <yahia@mrxprt.com>
2017-10-03 19:17:07 +0300

    [ Fix ] Clean ImageView Before Show the other image

--- JodelChallenge/Views/CustomViews/CollectionCell/CollectionCell.swift
+++ JodelChallenge/Views/CustomViews/CollectionCell/CollectionCell.swift
@@ -19,9 +19,10 @@ class CollectionCell : UICollectionViewCell{
         showTitle(title:photo.title)
     }
     func showImage(url:URL){
+        self.imageView.image = UIImage()
         let imageReauest:URLRequest = URLRequest.init(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
         self.activityIndicator.startAnimating()
-        self.imageView.setImageWith(imageReauest, placeholderImage: UIImage(), success:  { (reuqest, response, image) in
+        self.imageView.setImageWith(imageReauest, placeholderImage: nil, success:  { (reuqest, response, image) in
             self.activityIndicator.stopAnimating()
             self.imageView.image = image
             self.imageView.layer.contentsRect = CGRect(x:  0.2, y: 0.2, width: 0.7, height: 0.7)
